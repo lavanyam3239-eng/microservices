c4913401e55c02085df1e516690dc74c19acc66f
Lavanya Matta <lavanyam3239@gmail.com>
2026-04-11 13:00:20 -0400

    entity layer added

---
+++ src/main/java/com/microservices/pps/entity/Product.java
@@ -0,0 +1,22 @@
+package com.microservices.pps.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private double price;
+
+    private int stock;
+}
